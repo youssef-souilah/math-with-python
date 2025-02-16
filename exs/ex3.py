b0cbb2e210beacab827cac10104405e433d55349
Youssef Souilah <souilah.youssef.0@gmail.com>
2025-02-16 14:58:49 +0100

    modifier ex 3

--- exs/ex3.py
+++ exs/ex3.py
@@ -7,10 +7,10 @@ f= (x**2)+(y**2)+(z**2)+(x*y)+(y*z)+(z*x)+x+y+z
 df_dx=sp.diff(f,x)
 df_dy=sp.diff(f,y)
 df_dz=sp.diff(f,z)
-fun_df_dx=sp.lambdify((x,y,z),df_dx,"numpy")
-fun_df_dy=sp.lambdify((x,y,z),df_dy,"numpy")
-fun_df_dz=sp.lambdify((x,y,z),df_dz,"numpy")
-fun_f=sp.lambdify((x,y,z),f,"numpy")
+fun_df_dx=sp.lambdify((x,y,z),df_dx)
+fun_df_dy=sp.lambdify((x,y,z),df_dy)
+fun_df_dz=sp.lambdify((x,y,z),df_dz)
+fun_f=sp.lambdify((x,y,z),f)
 
 def des_grad(fun_df_dx,fun_df_dy,fun_df_dz,alpha,max):
     cur_x,cur_y,cur_z=0,0,0
@@ -28,12 +28,37 @@ def des_grad(fun_df_dx,fun_df_dy,fun_df_dz,alpha,max):
         
     return points
 
+
+def tracer_3d(points, fun_f):
+    X, Y, Z = np.meshgrid(
+        np.linspace(-20, 20, 100),
+        np.linspace(-20, 20, 100),
+        np.linspace(-20, 20, 100)
+    )
+    W = fun_f(X, Y, Z)
+    
+    fig = plt.figure(figsize=(10, 7))
+    ax = fig.add_subplot(111, projection='3d')
+    
+    px, py, pz = zip(*points)
+    pw = [fun_f(x, y, z) for x, y, z in points]
+    ax.scatter(px, py, pz, c=pw, cmap='plasma', marker='o', label='Descente de gradient')
+    
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.set_title('Descente de gradient pour f(x, y, z)')
+    ax.legend()
+    
+    plt.show()
+
+
 # parametres
 print("Descente de Gradient pour f(x,y,z)")
 alpha = float(input('Entrer le taux d\'apprentissage alpha :')) 
 max_iter = int(input('Entrer le nombre maximal d\'itérations max:')) 
 if (alpha>0 and max_iter>=20):
-    print(des_grad(fun_df_dx,fun_df_dy,fun_df_dz,alpha,max_iter))
+    tracer_3d(des_grad(fun_df_dx,fun_df_dy,fun_df_dz,alpha,max_iter),fun_f)
 else:
     print('alpha doit etre positif ! et le max doit etre >=20 !!!!')
 
